5f0f4b982e1f6732210e6c0d8da2cd500c38e984
keery <mon-compte1131@live.fr>
2019-02-19 21:20:58 +0100

    Adding background circle

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -15,15 +15,21 @@ class App extends Component {
             <div className="columns is-centered is-vcentered full-height">
               <Generator />
               <div className="shape-wrapper">
-                <svg xmlns="http://www.w3.org/2000/svg" width="839" height="783" viewBox="0 0 839 783" className="shape shape-1">
+                <svg xmlns="http://www.w3.org/2000/svg" width="839" height="783" viewBox="0 0 839 783" id='shape-1' className="shape">
                   <path d="M417,9C581.813,25.749,841,231.508,839,361S745,609.688,669,678,467,857.556,237,708,3,523.361,1,403-11,235.147,125,107,299-2.992,417,9Z"/>
                 </svg>
-                <svg xmlns="http://www.w3.org/2000/svg" width="632" height="669" viewBox="0 0 632 669" className="shape shape-2">
+                <svg xmlns="http://www.w3.org/2000/svg" width="632" height="669" viewBox="0 0 632 669" id='shape-2' className="shape">
                   <path d="M386,2c96.9,36.313,196,224.529,212,265s54,103.656,20,162S508,564.756,468,597s-86,93.757-168,64S106,584.7,76,569,0,494.583,0,439,22,270.44,30,226s10-89.626,32-114S188,52.32,254,32,370-4,386,2Z"/>
                 </svg>
-                <svg xmlns="http://www.w3.org/2000/svg" width="839" height="783" viewBox="0 0 839 783" className="shape shape-3">
-                <path d="M386,2c96.9,36.313,196,224.529,212,265s54,103.656,20,162S508,564.756,468,597s-86,93.757-168,64S106,584.7,76,569,0,494.583,0,439,22,270.44,30,226s10-89.626,32-114S188,52.32,254,32,370-4,386,2Z"/>
+                <svg xmlns="http://www.w3.org/2000/svg" width="839" height="783" viewBox="0 0 839 783" id='shape-3' className="shape">
+                  <path d="M386,2c96.9,36.313,196,224.529,212,265s54,103.656,20,162S508,564.756,468,597s-86,93.757-168,64S106,584.7,76,569,0,494.583,0,439,22,270.44,30,226s10-89.626,32-114S188,52.32,254,32,370-4,386,2Z"/>
                 </svg>
+                <div id='circle-1' className='circle' />
+                <div id='circle-2' className='circle' />
+                <div id='circle-3' className='circle' />
+                <div id='circle-4' className='circle' />
+                <div id='circle-5' className='circle' />
+                <div id='shadow-card' />
               </div>
             </div>
           </div>
