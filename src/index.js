f9551c8bb50e5fedc4bc34d904edec89dbd960b2
Guillaume Esnault <mon-compte1131@live.fr>
2019-09-09 17:47:03 +0200

    feat: integration Sentry

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from '@sentry/browser';
 import App from './containers/App/App';
 import registerServiceWorker from './registerServiceWorker';
 import './i18n/setupI18n';
+
+Sentry.init({ dsn : process.env.REACT_APP_DSN_SENTRY_CLIENT });
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
