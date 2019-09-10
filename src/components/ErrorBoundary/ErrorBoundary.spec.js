561c067f5c607a25b9a96498629685f6f061e353
Guillaume Esnault <mon-compte1131@live.fr>
2019-09-10 15:57:15 +0200

    test: <ErrorBoundary />

---
+++ src/components/ErrorBoundary/ErrorBoundary.spec.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import * as Sentry from '@sentry/browser';
+import ErrorBoundary from './ErrorBoundary';
+
+jest.mock('react-i18next', () => ({
+    withTranslation : () => Component => props => <Component t={jest.fn(t => t)} {...props} />,
+}));
+
+Sentry.showReportDialog = jest.fn();
+
+const TestComponent = () => (
+    <div>Test</div>
+);
+
+const throwChildrenError = (wrapper) => {
+    const children = wrapper.find(TestComponent);
+    const error = new Error('test error');
+    children.simulateError(error);
+};
+
+describe('ErrorBoundary Component Unit Test', () => {
+    let wrapper;
+
+    beforeEach(() => {
+        wrapper = mount(<ErrorBoundary><TestComponent /></ErrorBoundary>).find('ErrorBoundary');
+    });
+
+    it('should at least render Content', () => {
+        expect(wrapper.exists()).toEqual(true);
+    });
+
+    it('should catch when error is throw', () => {
+        const spy = jest.spyOn(wrapper.instance(), 'componentDidCatch');
+        throwChildrenError(wrapper);
+        expect(wrapper.instance().state).toMatchObject({ hasError : true });
+        expect(spy).toHaveBeenCalled();
+    });
+
+    it('should open Sentry popup on click', () => {
+        throwChildrenError(wrapper);
+        wrapper = wrapper.update();
+        const button = wrapper.find('button.gradient-btn');
+        expect(button.exists()).toBeTruthy();
+        button.simulate('click');
+        expect(Sentry.showReportDialog).toHaveBeenCalled();
+    });
+});
