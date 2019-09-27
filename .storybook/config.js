129af01559756e17d0e10c72347d206fb3c3ab18
Guillaume Esnault <mon-compte1131@live.fr>
2019-09-27 17:30:42 +0200

    refactor: config storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -11,14 +11,18 @@ export default function SuspenseWrapper({ children }) {
     return <Suspense fallback={<div>loading...</div>}>{children}</Suspense>;
 }
 
+// Addon which show component's JSX
 setAddon(JSONAddon);
+
+// Show information about your component like proptypes
 addDecorator(withInfo);
-addDecorator(story => <SuspenseWrapper>{story()}</SuspenseWrapper>)
 addDecorator((storyFn) => (
-    <I18nextProvider i18n={i18n}>
-        <div style={{ textAlign: 'center' }}>
-            {storyFn()}
-        </div>
-    </I18nextProvider>
+    <SuspenseWrapper>
+        <I18nextProvider i18n={i18n}>
+            <div style={{ textAlign: 'center' }}>
+                {storyFn()}
+            </div>
+        </I18nextProvider>
+    </SuspenseWrapper>
 ));
 configure(require.context('../src', true, /\.stories\.js$/), module);
