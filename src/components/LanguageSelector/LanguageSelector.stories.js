02cb11c5d6007c766b74dd0de8983abcdcec87e7
Guillaume Esnault <mon-compte1131@live.fr>
2019-09-27 16:59:25 +0200

    feat: add storybook addon JSX

--- src/components/LanguageSelector/LanguageSelector.stories.js
+++ src/components/LanguageSelector/LanguageSelector.stories.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+
 import LanguageSelector from './LanguageSelector';
 
 storiesOf('LanguageSelector', module) 
-    .add('is opened', () => <LanguageSelector name="Test" isOpen />);
+    .addWithJSX('is opened', () => <LanguageSelector name="Test" isOpen />);
