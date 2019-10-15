166426b9bb6955c28069a50216735b5c38352725
Guillaume Esnault <mon-compte1131@live.fr>
2019-10-15 10:34:30 +0200

    css: close modal

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { withTranslation } from 'react-i18next';
+import './Modal.scss';
 
-const Modal = ({ content, closeModal }) => (
-    <div className="Loader">
-        <div className="loader-container">
-            <button onClick={closeModal}>X</button>
+const Modal = ({ content, closeModal, t }) => (
+    <div className="Modal">
+        <div className="modal-closing-area" />
+        <div className="modal-container">
+            <button
+                id="modal-close"
+                onClick={closeModal}
+                type="button"
+                title={t('close')}
+            >
+                <i className="fas fa-times" />
+            </button>
             { content }
         </div>
     </div>
@@ -11,6 +22,9 @@ const Modal = ({ content, closeModal }) => (
 
 
 Modal.propTypes = {
+    content    : PropTypes.node.isRequired,
+    closeModal : PropTypes.func.isRequired,
+    t          : PropTypes.func.isRequired,
 };
 
-export default Modal;
+export default withTranslation('index')(Modal);
