11f54236c3fd3ae1ba69e23cc4affe70901ac5bf
Guillaume Esnault <mon-compte1131@live.fr>
2019-10-17 15:00:55 +0200

    feat: check if file match with given format

--- server/middlewares/multer.js
+++ server/middlewares/multer.js
@@ -4,25 +4,40 @@ import { IMAGES_PATH } from '../const'
 import logger from '../logger';
 
 const getDestinationAccordingType = (type) => {
-    switch (type) {
-        case 'images':
-            return IMAGES_PATH;
-        default:
-            logger.error('No destination found');
-            return false;
-    }
+  switch (type) {
+      case 'images':
+          return IMAGES_PATH;
+      default:
+          logger.error('No destination found');
+          return false;
+  }
 };
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, getDestinationAccordingType(req.params.type))
-    },
-    filename: (req, file, cb) => {
-      cb(null, Date.now() + path.extname(file.originalname))
-    }
+  destination: (req, file, cb) => {
+    cb(null, getDestinationAccordingType(req.params.type))
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname))
+  }
 });
 
-export default multer({ storage, limits : {
-  fileSize : 1000000,
-  files    : 1
-}});
+const fileFilter = (req, file, cb) => {
+  const filetypes = /jpeg|jpg|png|gif/;
+  const mimetype = filetypes.test(file.mimetype);
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+
+  if (mimetype && extname) {
+    return cb(null, true);
+  }
+  return cb(`File upload only supports the following filetypes - ${filetypes}`);
+};
+
+export default multer({
+  storage,
+  fileFilter,
+  limits : {
+    fileSize : 1000000,
+    files    : 1
+  }
+});
