a562cf9938d3c862787ff179a0a1356dcb56b2e8
Guillaume Esnault <mon-compte1131@live.fr>
2019-10-25 18:45:05 +0200

    refactor: upload img on aws

--- server/middlewares/getUppyOptions.js
+++ server/middlewares/getUppyOptions.js
@@ -10,6 +10,13 @@ export default () => {
                 key    : INSTA_KEY,
                 secret : INSTA_SECRET
             },
+            s3 : {
+                getKey: (req, filename) => `images/${Math.random().toString(32).slice(2)}/${filename}`,
+                key    : 'AKIA6EQI76BV4DJ2AX5X',
+                secret : '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',
+                bucket : 'files-pokemon',
+                region : 'eu-west-3',
+            }
         },
         server : {
             host     : `${HOST}:${PORT}`,
--- server/routes/index.js
+++ server/routes/index.js
@@ -3,6 +3,5 @@ import api from './api';
 import { IMAGES_PATH } from '../const';
 
 export default (app) => {
-    app.use('/static/images', express.static(IMAGES_PATH));
     app.use('/api', api);
 };
