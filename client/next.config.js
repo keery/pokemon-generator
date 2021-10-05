9f25221272b94556a6d27f798080e57e675e4893
Guillaume <contact@guillaumeesnault.fr>
2021-10-05 18:09:54 -0400

    chore: clean next.config

--- client/next.config.js
+++ client/next.config.js
@@ -1,38 +1,15 @@
-const { i18n } = require('./next-i18next.config')
+const { i18n } = require("./next-i18next.config");
 
-const env = process.env.NODE_ENV || 'development'
-
-let envVars = {}
+let envVars = {};
 
 module.exports = {
   i18n,
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: '/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          {
-            key: 'Access-Control-Allow-Methods',
-            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
-          },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value:
-              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-          },
-        ],
-      },
-    ]
-  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
       use: [
         {
-          loader: '@svgr/webpack',
+          loader: "@svgr/webpack",
           options: {
             svgo: false,
             svgoConfig: {
@@ -41,10 +18,10 @@ module.exports = {
           },
         },
       ],
-    })
+    });
 
-    return config
+    return config;
   },
-  devtool: 'inline-source-map',
+  devtool: "inline-source-map",
   env: envVars,
-}
+};
