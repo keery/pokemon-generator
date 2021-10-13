945772111625f100a44d42938c2e578742d8a775
Guillaume <contact@guillaumeesnault.fr>
2021-10-13 12:11:41 -0400

    fix: clear localstorage  on error

--- client/pages/_error.tsx
+++ client/pages/_error.tsx
@@ -1,17 +1,18 @@
-import React from 'react'
-import NextErrorPage from 'next/error'
-import { NextPage } from 'next'
+import React from "react";
+import NextErrorPage from "next/error";
+import { NextPage } from "next";
 
 const ErrorPage: NextPage<{ statusCode: number }> = ({
   statusCode,
 }: {
-  statusCode: number
+  statusCode: number;
 }) => {
-  return <NextErrorPage statusCode={statusCode} />
-}
+  localStorage.removeItem(process.env.NEXT_PUBLIC_KEY_CACHE);
+  return <NextErrorPage statusCode={statusCode} />;
+};
 
 ErrorPage.getInitialProps = async (ctx) => {
-  return NextErrorPage.getInitialProps(ctx)
-}
+  return NextErrorPage.getInitialProps(ctx);
+};
 
-export default ErrorPage
+export default ErrorPage;
