372edb41855b280bd6214bd3160fcc348483d74b
Guillaume <contact@guillaumeesnault.fr>
2021-10-28 15:56:42 -0400

    fix: allow all cors

--- back/src/main.ts
+++ back/src/main.ts
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3002
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule)
   app.enableCors({
-    origin: '*',
+    origin: 'https://card-pokemon.com',
   })
   app.useGlobalPipes(new ValidationPipe())
   app.setGlobalPrefix('/api')
