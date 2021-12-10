c6475aeefd231820233e346dc4f6b6640f9f0cd2
Guillaume <contact@guillaumeesnault.fr>
2021-12-10 16:55:27 -0500

    feat: default language en

--- client/next-i18next.config.js
+++ client/next-i18next.config.js
@@ -1,12 +1,12 @@
-const path = require('path')
+const path = require("path");
 
 module.exports = {
   i18n: {
-    defaultLocale: 'fr',
-    locales: ['fr', 'en'],
-    localePath: path.resolve('./public/locales'),
+    defaultLocale: "en",
+    locales: ["fr", "en"],
+    localePath: path.resolve("./public/locales"),
   },
   react: {
     useSuspense: false,
   },
-}
+};
