ba7d40b29a6906f237cee2d333f978c8610b68a8
Guillaume <contact@guillaumeesnault.fr>
2022-11-02 14:07:15 +0100

    fix: remove dark bg from input and textarea pixel mode

--- client/theme/components/Textarea.ts
+++ client/theme/components/Textarea.ts
@@ -19,6 +19,7 @@ const Textarea = {
       borderStyle: "solid",
       borderWidth: "4px",
       fontSize: "xs",
+      backgroundColor: "white",
       borderImageSlice: 2,
       borderImageWidth: 2,
       borderImageSource: `url('data:image/svg+xml;utf8,<?xml version="1.0" encoding="UTF-8" ?><svg version="1.1" width="5" height="5" xmlns="http://www.w3.org/2000/svg"><path d="M2 1 h1 v1 h-1 z M1 2 h1 v1 h-1 z M3 2 h1 v1 h-1 z M2 3 h1 v1 h-1 z" fill="rgb(33,37,41)" /></svg>')`,
