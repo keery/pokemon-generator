c9f03cd63f541379aefbe615f302bbdb05780b5f
Guillaume <contact@guillaumeesnault.fr>
2022-11-06 01:34:09 +0100

    fix: countdown day number

--- client/hooks/useCountdown.ts
+++ client/hooks/useCountdown.ts
@@ -36,5 +36,5 @@ const getReturnValues = (countDown): CountdownTime => {
   const minutes = Math.floor((countDown % (1000 * 60 * 60)) / (1000 * 60));
   const seconds = Math.floor((countDown % (1000 * 60)) / 1000);
 
-  return { days, hours, minutes, seconds };
+  return { days: days === 7 ? 0 : 7, hours, minutes, seconds };
 };
