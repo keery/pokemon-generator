de5cd3adca8bc3008925c11f1033622986fe4e8a
Guillaume <contact@guillaumeesnault.fr>
2022-11-29 13:27:28 +1100

    feat: position gallery top cards on mobile

--- client/components/Gallery/GalleryTopBackground.tsx
+++ client/components/Gallery/GalleryTopBackground.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Flex } from "@chakra-ui/react";
+import { Flex, useBreakpointValue } from "@chakra-ui/react";
 import Blob from "public/assets/img/blob.svg";
 import { screenPercent } from "~utils/helper";
 import ParallaxY, { ParallaxYProps } from "~components/ParallaxY";
@@ -19,7 +19,105 @@ const CardBackground = (props: ParallaxYProps) => {
   );
 };
 
+const DEFAULT_PROPS = { x: 0, height: 0, width: 0 };
+
 const GalleryTopBackground = () => {
+  const card1 =
+    useBreakpointValue({
+      base: {
+        x: "6.4rem",
+        height: "5.5rem",
+        width: "7.6rem",
+      },
+      sm: {
+        x: "11.7rem",
+        height: "8.5rem",
+        width: "10.6rem",
+      },
+      md: {
+        x: "17rem",
+        height: "9.2rem",
+        width: "11.15rem",
+      },
+      lg: {
+        x: "24.8rem",
+        height: "10rem",
+        width: "11.95rem",
+      },
+    }) || DEFAULT_PROPS;
+
+  const card2 =
+    useBreakpointValue({
+      base: {
+        x: "8rem",
+        height: "12rem",
+        width: "8.25rem",
+      },
+      sm: {
+        x: "15rem",
+        height: "17rem",
+        width: "12.25rem",
+      },
+      md: {
+        x: "19.5rem",
+        height: "18.7rem",
+        width: "13.85rem",
+      },
+      lg: {
+        x: "26rem",
+        height: "23.5rem",
+        width: "16.55rem",
+      },
+    }) || DEFAULT_PROPS;
+
+  const card3 =
+    useBreakpointValue({
+      base: {
+        x: "5.2rem",
+        height: "16.7rem",
+        width: "12.3rem",
+      },
+      sm: {
+        x: "9.2rem",
+        height: "21.7rem",
+        width: "16.3rem",
+      },
+      md: {
+        x: "10rem",
+        height: "22.5rem",
+        width: "17.65rem",
+      },
+      lg: {
+        x: "10rem",
+        height: "25.5rem",
+        width: "19.65rem",
+      },
+    }) || DEFAULT_PROPS;
+
+  const card4 =
+    useBreakpointValue({
+      base: {
+        x: "8rem",
+        height: "17rem",
+        width: "13.4rem",
+      },
+      sm: {
+        x: "12rem",
+        height: "26rem",
+        width: "20.4rem",
+      },
+      md: {
+        x: "13rem",
+        height: "29.5rem",
+        width: "23rem",
+      },
+      lg: {
+        x: "13rem",
+        height: "29.5rem",
+        width: "23rem",
+      },
+    }) || DEFAULT_PROPS;
+
   return (
     <Flex pos="absolute" left="0" top="0" right="0" h="100vh">
       <Flex
@@ -39,45 +137,45 @@ const GalleryTopBackground = () => {
         input={[0, screenPercent(125)]}
         output={[screenPercent(-33), screenPercent(10)]}
         style={{
-          x: "calc(-50% - 24.8rem)",
+          x: `calc(-50% - ${card1.x})`,
           rotate: "-8.26deg",
         }}
-        height="10rem"
-        width="11.95rem"
+        height={card1.height}
+        width={card1.width}
         bgImage="/assets/img/gallery-top/pokeball-purple.webp"
       />
       <CardBackground
         input={[0, screenPercent(125)]}
         output={[screenPercent(-33), screenPercent(1)]}
         style={{
-          x: "calc(-50% + 26rem)",
+          x: `calc(-50% + ${card2.x})`,
           rotate: "7.54deg",
         }}
-        height="23.5rem"
-        width="16.55rem"
+        height={card2.height}
+        width={card2.width}
         bgImage="/assets/img/gallery-top/mew.webp"
       />
       <CardBackground
         input={[0, screenPercent(125)]}
         output={[screenPercent(13), screenPercent(-20)]}
         style={{
-          x: "calc(-50% - 10rem)",
+          x: `calc(-50% - ${card3.x})`,
           rotate: "-5.27deg",
         }}
         bgImage="/assets/img/gallery-top/florizare.webp"
-        height="25.5rem"
-        width="19.65rem"
+        height={card3.height}
+        width={card3.width}
       />
       <CardBackground
         input={[0, screenPercent(125)]}
         output={[screenPercent(13), screenPercent(-3)]}
         style={{
-          x: "calc(-50% + 13rem)",
+          x: `calc(-50% + ${card4.x})`,
           rotate: "7.79deg",
           scale: "0.35",
         }}
-        height="555px"
-        width="400px"
+        height={card4.height}
+        width={card4.width}
         borderRadius="3rem"
         bgImage="https://cdn.dribbble.com/users/1147613/screenshots/15390615/media/0197f9d885df23a6ab039eedebc40f5b.png?compress=1&resize=1600x1200&vertical=top"
       />
